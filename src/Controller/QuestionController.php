00e86875166849aab64dbfa2b06a7cec59319aac
aqibmehraj20 <aqib@yarikul.in>
2021-08-24 14:43:38 +0530

    init commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response("What a controller we have conjured!");
+    }
 
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+
+        return new Response(sprintf('Future page to show the questions "%s" !',
+       ucwords(str_replace('-',' ', $slug))
+        ));
+    }
 }
